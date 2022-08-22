21426e14519af33b7ba2705295bfe79228108f25
Ryan Wall <rwall9@its.jnj.com>
2022-08-22 16:53:21 -0400

    wrapping, typehints, and docstring mods to time.py.
    Also found a reference to JanssenPI which I changed back

--- PIconnect/time.py
+++ PIconnect/time.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+from typing import Union
 import numpy as np
 import pytz
 
@@ -6,21 +7,26 @@ from PIconnect.AFSDK import AF
 from PIconnect.config import PIConfig
 
 
-def to_af_time_range(start_time, end_time):
+def to_af_time_range(
+    start_time: Union[str, datetime.datetime],
+    end_time: Union[str, datetime.datetime, float],
+) -> AF.Time.AFTimeRange:
     """Convert a combination of start and end time to a time range.
 
-    Both `start_time` and `end_time` can be either a :any:`datetime.datetime` object or a string.
-    `datetime` objects are first converted to a string, before being passed to
-    :afsdk:`AF.Time.AFTimeRange <M_OSIsoft_AF_Time_AFTimeRange__ctor_1.htm>`. It is also
-    possible to specify either end as a `datetime` object, and then specify the other
+    Both `start_time` and `end_time` can be either a :any:`datetime.datetime`
+    object or a string `datetime` objects are first converted to a string,
+    before being passed to :afsdk:`AF.Time.AFTimeRange
+    <M_OSIsoft_AF_Time_AFTimeRange__ctor_1.htm>`. It is also possible to
+    specify either end as a `datetime` object, and then specify the other
     boundary as a relative string.
 
     Args:
-        start_time (str | datetime): Start time of the time range.
-        end_time (str | datetime): End time of the time range.
+        start_time (Union[str, datetime.datetime]): start time
+        end_time (Union[str, datetime.datetime, float]): end time if str or
+            datetime object. Else if float will use current time.
 
     Returns:
-        :afsdk:`AF.Time.AFTimeRange <M_OSIsoft_AF_Time_AFTimeRange__ctor_1.htm>`: Time range covered by the start and end time.
+        AF.Time.AFTimeRange:  Time range covered by the start and end time.
     """
     if isinstance(start_time, datetime):
         start_time = start_time.isoformat()
@@ -38,14 +44,15 @@ def to_af_time_range(start_time, end_time):
     return AF.Time.AFTimeRange(start_time, end_time)
 
 
-def to_af_time(time):
+def to_af_time(time: Union[str, datetime.datetime]) -> AF.Time.AFTime:
     """Convert a time to a AFTime value.
 
     Args:
-        time (str | datetime): Time to convert to AFTime.
+        time (Union[str,datetime.datetime]): Time to convert to AFTime.
 
     Returns:
-        :afsdk:`AF.Time.AFTime <M_OSIsoft_AF_Time_AFTime__ctor_7.htm>`: Time range covered by the start and end time.
+        :afsdk:`AF.Time.AFTime <M_OSIsoft_AF_Time_AFTime__ctor_7.htm>`: Time
+            range covered by the start and end time.
     """
     if isinstance(time, datetime):
         time = time.isoformat()
@@ -59,21 +66,28 @@ def timestamp_to_index(timestamp):
     """Convert AFTime object to datetime in local timezone.
 
     Args:
-        timestamp (`System.DateTime`): Timestamp in .NET format to convert to `datetime`.
+        timestamp (`System.DateTime`): Timestamp in .NET format to convert to
+            `datetime`.
 
     Returns:
-        `datetime`: Datetime with the timezone info from :data:`PIConfig.DEFAULT_TIMEZONE <JanssenPI.config.PIConfigContainer.DEFAULT_TIMEZONE>`.
+        `datetime`: Datetime with the timezone info from
+        :data:`PIConfig.DEFAULT_TIMEZONE
+        <PIconnect.config.PIConfigContainer.DEFAULT_TIMEZONE>`.
     """
-    try:  # issue of converting infite endtimes, now defaulted to timezone unaware infinite timezone
-        if datetime(
-            timestamp.Year,
-            timestamp.Month,
-            timestamp.Day,
-            timestamp.Hour,
-            timestamp.Minute,
-            timestamp.Second,
-            timestamp.Millisecond * 1000,
-        ) == datetime(9999, 12, 31, 23, 59, 59):
+    try:  # issue of converting infite endtimes, now defaulted to timezone
+        # unaware infinite timezone
+        if (
+            datetime(
+                timestamp.Year,
+                timestamp.Month,
+                timestamp.Day,
+                timestamp.Hour,
+                timestamp.Minute,
+                timestamp.Second,
+                timestamp.Millisecond * 1000,
+            )
+            == datetime(9999, 12, 31, 23, 59, 59)
+        ):
             return np.nan
 
         else:
