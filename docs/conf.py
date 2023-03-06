65edf6864be357c1d17b147606fd44d14c61bbb1
Ryan Wall <rwall9@its.jnj.com>
2023-03-06 16:57:53 -0500

    tox works!

--- docs/conf.py
+++ docs/conf.py
@@ -99,7 +99,7 @@ copyright = "2023, Ryan Wall"
 # If you don’t need the separation provided between version and release,
 # just set them both to the same value.
 try:
-    from piconnect import __version__ as version
+    from PIconnect import __version__ as version
 except ImportError:
     version = ""
 
